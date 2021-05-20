ace01ada519a42494f312aeb9ce0a60d4e35171f
Tristan Jay <capdincrando@gmail.com>
2021-05-20 12:28:26 -0500

    Forgot to add .txt

--- autoStart.lua
+++ autoStart.lua
@@ -4,13 +4,13 @@ local serialization = require("serialization")
 
 -- Download file list
 local header = "https://raw.githubusercontent.com/CapdinCrando/SiberiaATMSystem/master/"
-shell.execute("wget -f" .. header .. "downloadList")
+shell.execute("wget -f" .. header .. "downloadList.txt")
 
 -- Download Files
-local tableFile = assert(io.open("downloadList"))
+local tableFile = assert(io.open("downloadList.txt"))
 local files = serialization.unserialize(tableFile:read("*all"))
 for _,f in ipairs(files) do
-	shell.execute("wget " .. header .. f)
+	shell.execute("wget -f" .. header .. f)
 end
 
 -- Start server
