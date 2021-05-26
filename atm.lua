76a343432d7332805950144341436d9dddf2e591
Tristan Jay <capdincrando@gmail.com>
2021-05-26 08:14:04 -0500

    Update atm.lua

--- atm.lua
+++ atm.lua
@@ -5,6 +5,8 @@ local term = require("term")
 local component = require("component")
 local gpu = component.gpu
 
+local bankName = ""
+
 local function handleAccountCreation(signal, _, name)
 	print(name)
 end
@@ -14,36 +16,102 @@ event.listen("bioReader", handleAccountCreation)
 local function checkBalance()
 	api.clear()
 	api.clearTable()
-	api.label(1, 1, "Check balance")
+	api.label(1, 1, "Please swipe card again to check balance")
+	api.screen()
+
+	local a,b,c,d,e = event.pull("magData")
+	local response = accountApi.getAmount(c)
+	api.clear()
+	api.clearTable()
+	if response == "Account does not exist!" then
+		api.label(1, 1, response)
+	else
+		api.label(1, 1, " Account balance: $" .. response ..)
+	end
+	
+	api.setTable("Return", chooseScreen, 16,29,7,9)
+	api.screen()
+
+	local _, _, x, y = event.pull("touch")
+	api.checkxy(x, y)
 end
 
 local function withdrawl()
 	api.clear()
 	api.clearTable()
-	api.label(1, 1, "Withdrawl")
+	api.label(1, 1, "Enter amount to withdrawl: $")
+	api.screen()
+	amount = io.read("*line")
+
+	api.clear()
+	api.label(1, 1, "Please swipe card to withdrawl amount")
+	api.screen()
+	local a,b,c,d,e = event.pull("magData")
+
+	api.clear()
+	-- Check amount in ATM
+	local response = accountApi.transfer(c, nil, tonumber(amount))
+	if response == "Transaction successful!" then
+		
+		api.label(1, 3, "Please take your money!")
+		-- Give cash
+	end
+	api.label(1, 1, response)
+	
+	api.setTable("Return", chooseScreen, 16,29,7,9)
+	api.screen()
+
+	local _, _, x, y = event.pull("touch")
+	api.checkxy(x, y)
 end
 
 local function deposit()
 	api.clear()
 	api.clearTable()
-	api.label(1, 1, "Deposit")
+	-- Give box
+	api.label(1, 1, "Please deposit cash in box")
+	api.label(1, 3, "Swipe card to deposit")
+	api.label(28, 7, "-->")
+	api.screen()
+
+	-- Take box and calculate amount
+	local amount = 1
+
+	api.clear()
+	api.label(1, 1, "Please swipe card to withdrawl amount")
+	api.screen()
+	local a,b,c,d,e = event.pull("magData")
+
+	api.clear()
+	-- Check amount in ATM
+	local response = accountApi.transfer(nil, c, tonumber(amount))
+	if response == "Transaction successful!" then
+		
+		api.label(1, 3, "Please take your money!")
+		-- Give cash
+	end
+	api.label(1, 1, response)
+	
+	api.setTable("Return", chooseScreen, 16,29,7,9)
+	api.screen()
+
+	local _, _, x, y = event.pull("touch")
+	api.checkxy(x, y)
 end
 
-local isUsing = true
 local function exit()
 	api.clear()
 	api.clearTable()
-	api.label(1, 10, "Check balance")
+	api.label(1, 1, "Thank you for using the Bank!")
 	-- Open door
 	os.sleep(5)
-	isUsing = false
 end
 
 local function chooseScreen()
 	api.clear()
 	api.clearTable()
-	api.label(1, 3, "Welcome, " .. c .. "!")
-	api.label(1, 3, "Please choose an action:")
+	api.label(3, 1, "Welcome, " .. c .. "!")
+	api.label(3, 2, "Please choose an action:")
 
 	api.setTable("Check $", checkBalance, 2,14,3,5)
 	api.setTable("Withdrawl", withdrawl, 16,29,3,5)
@@ -62,12 +130,12 @@ term.setCursorBlink(false)
 while true do
 	-- State 1
 	api.clear()
-	api.label(1, 3, "Welcome to Siberia!")
+	api.label(1, 3, "Welcome to the BankTM!")
 	api.label(1, 5, "Please insert card to continue")
 	api.label(28, 7, "-->")
 	
 	-- State 2
-	a,b,c,d,e = event.pull("magData")
+	local a,b,c,d,e = event.pull("magData")
 	-- Needs to check account
 	-- Close door if good
 	chooseScreen()
@@ -79,13 +147,3 @@ while true do
 		
 	end
 end
-
--- Handle create account event
-
--- Access card event
-
--- Present options
-
--- Options (Withdrawl, Deposit, Check Balance)
-
--- Return
